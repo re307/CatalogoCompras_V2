f12d5577602b02798ae01465d33065eb1f81b526
Rafael Eduardo Méndez Pérez <rmendez@weecompany.net>
2023-08-27 22:42:49 -0600

    Grit listo
    Se deja listo el grid con los rubros de los productos.

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,79 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Icon from 'fa-svelte'
+  import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons/faArrowAltCircleLeft';
+  import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons/faArrowAltCircleRight';
+  import Producto from "./lib/Producto.svelte";
+  
+  let icon = faArrowAltCircleLeft;
+  let icon2 = faArrowAltCircleRight;
+  let catalogo = [];
+  let totalProductos = 0;
+  let cantidadProductos = 20;
+  let datoIniCarga = 0;
+  let datoFinCarga = 0;
+  let pagina = 1;
+  let totalPaginas = 0;
+  const infoRecibida = async () => {
+    const respuesta = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${pagina}`
+    );
+    const info = await respuesta.json();
+    totalProductos = info.info.count;
+    totalPaginas = Math.ceil(totalProductos / cantidadProductos);
+    catalogo = info.results;
+    datoIniCarga = catalogo[0].id;
+    datoFinCarga = catalogo[catalogo.length - 1].id;
+  };
+  infoRecibida();
+  const paginaSiguiente = () => {
+    pagina++;
+    if (pagina <= totalPaginas) {
+      infoRecibida();
+    } else {
+      pagina = totalPaginas;
+    }
+  };
+  const paginaAnterior = () => {
+    pagina--;
+    if (pagina >= 1) {
+      infoRecibida();
+    } else {
+      document.getElementById('anterior').disabled = true;
+      pagina = 1;
+    }
+  };
 </script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div>
+  <Icon icon={icon}>
+  </Icon></div>
+<h1 class="title">Tu catalogo</h1>
+<div class="container">
+  <div class="paginador">
+    <div id="anterior"
+      on:click={() => {
+        paginaAnterior();
+      }}
+    disabled>
+    <Icon icon={icon}>
+    </Icon>
+      Previos
+    </div>
+    <div>{datoIniCarga}-{datoFinCarga} de {totalProductos}</div>
+    <div id="siguiente"
+      on:click={() => {
+        paginaSiguiente();
+      }}
+      disabled={pagina == totalPaginas}
+    >
+      Siguientes
+      <Icon icon={icon2}>
+      </Icon>
+    </div></div>
+</div>
+<div class="container">
+  <div class="grid">
+    {#each catalogo as producto}
+      <Producto {producto} />
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
---
+++ src/lib/Producto.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let producto
+</script>
+
+<div>
+    <img src={producto.image} />
+    <h1>Nombre: {producto.name}</h1>
+    <h3>Especie: {producto.species}</h3>
+</div>
